78b01f4548f384b80c112d010de2d43862cfe212
riccardofano <rcr.fano@gmail.com>
2022-07-25 14:56:05 +0200

    style App

--- src/App.tsx
+++ src/App.tsx
@@ -30,17 +30,17 @@ function App() {
   };
 
   return (
-    <main className="container grid">
-      <div>
-        <div className="flex p-400 bg-blue-light br-md">
-          <img src="/image-jeremy.png" alt="" />
-          <h1 className="fs-h1 fw-light">
-            <span className="block text-neutral-300 fs-regular">Report for</span>
+    <main className="main-grid | container">
+      <header className="header">
+        <div className="header-info | flex p-400 bg-blue-light br-md">
+          <img className="profile" src="/image-jeremy.png" alt="" />
+          <h1 className="fs-h1 fw-light lh-sm">
+            <span className="block text-neutral-300 fs-regular fw-regular">Report for</span>
             Jeremy Robson
           </h1>
         </div>
 
-        <div className="backdrop | p-400 flex flex-sb bg-blue-dark text-neutral-300 br-md" data-backdrop="before">
+        <div className="timeframes | backdrop | p-400 flex bg-blue-dark text-neutral-400 br-md" data-backdrop="before">
           <button onClick={() => setTimeframe("daily")} data-active={isActive("daily")}>
             Daily
           </button>
@@ -51,7 +51,7 @@ function App() {
             Monthly
           </button>
         </div>
-      </div>
+      </header>
 
       {data?.map(({ title, timeframes }) => (
         <Card

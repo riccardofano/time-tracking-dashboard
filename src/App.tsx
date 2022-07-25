190b5cd475b1727644db861dc7cc5eaf95ab7a44
riccardofano <rcr.fano@gmail.com>
2022-07-25 10:03:28 +0200

    populate app with basic html structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
 import "./App.scss";
 
+import { ReactComponent as IconEllipses } from "./assets/icon-ellipsis.svg";
+import { ReactComponent as IconWork } from "./assets/icon-work.svg";
+
 function App() {
-  return <div></div>;
+  return (
+    <main>
+      <div>
+        <div>
+          <img src="/image-jeremy.png" alt="" />
+          <h1>
+            <span>Report for</span>
+            Jeremy Robson
+          </h1>
+        </div>
+        <div>
+          <p>Daily</p>
+          <p>Weekly</p>
+          <p>Monthly</p>
+        </div>
+      </div>
+
+      <article>
+        <div>
+          <IconWork />
+        </div>
+        <div>
+          <h2>Work</h2>
+          <button>
+            <span>Open menu</span>
+            <IconEllipses />
+          </button>
+        </div>
+        <div>
+          <h3>32hrs</h3>
+          <p>Last Week - 36hrs</p>
+        </div>
+      </article>
+    </main>
+  );
 }
 
 export default App;
